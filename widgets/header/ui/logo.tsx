8fc8dcae677b2061ee13053572d25c4a72fe8548
Михаил Журавлев <mihailzuravlev349@gmail.com>
2026-06-26 16:02:08 +0300

    update deploy config

--- widgets/header/ui/logo.tsx
+++ widgets/header/ui/logo.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 import LogoIcon from '@/shared/assets/logo.svg'
 import styles from './header.module.scss'
 export const Logo = () => {
     // Лого добавил как svg чтобы качество не терялось
     return <div className={styles.header__logo}>
-        <a href="/"><LogoIcon /></a>
+        <Link href="/"><LogoIcon /></Link>
     </div>
 }
